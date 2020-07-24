baadfad90e383b287e384f045ef22398e2651c1c
mohammad savadkohi <mohammadsavadkohi@mohammads-MBP.lan>
2020-07-25 03:39:40 +0430

    update

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import Header from './Header/Header'
 import Services from "./Services";
 import Card from './Card';
@@ -9,34 +9,34 @@ import Card from './Card';
 // import {HashRouter as Router } from "react-router-dom";
 
 
-function MainWrap(){
+function MainWrap() {
 
 
     return (
         <div className="main-wrap">
-<div>
-            <img src={require('./../Assests/header-parallax1.png')} alt="" className="header-img" />
-            <Header />
-            <div className="header-content">
-                <h2>پیام پارالاکس برای  کاربران خود</h2>
-                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
-                ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
-                و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی
-                مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+            <div>
+                <img src={require('./../Assests/header-parallax1.png')} alt="" className="header-img" />
+                <Header />
+                <div className="header-content">
+                    <h2>پیام پارالاکس برای  کاربران خود</h2>
+                    <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
+                    ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی
+                    مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
                 طراحان دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
-                <button>شروع به کار</button>
-            </div>
+                    <button>شروع به کار</button>
+                </div>
             </div>
             <div className="clear-both"></div>
             <Services />
 
-            <img src={require('./../Assests/p1.png')} alt=" " className="side-image1"/>
-            
+            <img src={require('./../Assests/p1.png')} alt=" " className="side-image1" />
+
             <Card />
-            
+
         </div>
     )
 
 }
 
-export default MainWrap ;
+export default MainWrap;
