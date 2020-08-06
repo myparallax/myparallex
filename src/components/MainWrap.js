a1d2376671925faa3dd3e07b14c5b526871286fa
Johnsavadkuhi <physitime@gmail.com>
2020-08-06 22:27:23 +0430

    update

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Home from './Home'
-import { BrowserRouter  , Route ,  Link , Switch } from 'react-router-dom'   
+//import Home from './Home'
+import { BrowserRouter  , Route  } from 'react-router-dom'   
 import Header from './Header/Header' 
 
 function MainWrap() {
