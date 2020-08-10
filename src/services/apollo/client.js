489fb558663317c2c5869274f33bc53b88098b9d
Johnsavadkuhi <physitime@gmail.com>
2020-08-11 04:07:54 +0430

    add cache

--- src/services/apollo/client.js
+++ src/services/apollo/client.js
@@ -1,9 +1,14 @@
 import ApolloClient from 'apollo-boost';
+import {InMemoryCache} from 'apollo-boost';
+
 
 const uri  = process.env.URI 
 
+const cache = new InMemoryCache() ; 
+
 const client = new ApolloClient({
-    uri 
+    uri , 
+    cache 
   });
 
 export default client 
