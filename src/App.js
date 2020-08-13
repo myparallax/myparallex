313b6c29da27d8d7e10492111c76f993caa27604
Johnsavadkuhi <physitime@gmail.com>
2020-08-13 18:50:51 +0430

    remove redux configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React, { useState } from "react";
-import { configureStore } from "./store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 // import components
 import MainWrap from './components/MainWrap';
 
 function App() {
-  const [store, ] = useState(configureStore().store);
 
   return (
     <>
-      <Provider store={store}>
-        <PersistGate
-          loading={null}
-          persistor={configureStore().persistor}
-        >
-
         <MainWrap />
-
-        </PersistGate>
-      </Provider>
+      \
     </>
   );
 } 
