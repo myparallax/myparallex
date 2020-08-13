64e180f379cbc2434342329403366ae372cbacc3
Johnsavadkuhi <physitime@gmail.com>
2020-08-13 18:19:35 +0430

    update

--- src/services/apollo/client.js
+++ src/services/apollo/client.js
@@ -1,14 +1,30 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { HttpLink } from 'apollo-boost';
 import {InMemoryCache} from 'apollo-boost';
+import { resolvers, typeDefs } from "./resolvers";
 
-
-const uri  = process.env.URI 
-
+// const uri  = process.env.URI 
+const uri = "http://localhost:4000" ; 
+console.log("uri : " +  uri );
 const cache = new InMemoryCache() ; 
 
 const client = new ApolloClient({
+  link : new HttpLink({
+    headers:{authorization:localStorage.getItem("token")}
+  }),
     uri , 
-    cache 
+    cache , 
+    typeDefs,
+    resolvers
   });
 
+  cache.writeData({
+    data: {
+      isLoggedIn: !!localStorage.getItem('token'),
+      
+    },
+  });
 export default client 
+
+
+
+
