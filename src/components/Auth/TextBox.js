d3de5804c6021212eb05ac5c950fd0b9d2e4a141
Johnsavadkuhi <physitime@gmail.com>
2020-08-15 01:57:13 +0430

    create TextBox as custome input

---
+++ src/components/Auth/TextBox.js
@@ -0,0 +1,31 @@
+import React  from "react";
+import { Label, Input } from "reactstrap";
+
+function TextBox(props) {
+
+    const {iconSrc , onChange ,
+    value , 
+    type , 
+    placeholder , 
+    name , 
+    className} = props ; 
+
+  return (
+    <>
+      <div className="inputs-text" >
+        <img src={iconSrc} alt=" " />
+        <Label htmlFor={type==="text" ? "username" : "password" }></Label>
+        <Input
+        onChange = {onChange} 
+        valu={value}
+        type={type}
+        placeholder={placeholder}
+        nam={name}
+        className={className}
+        />
+      </div>
+    </>
+  );
+}
+
+export default TextBox ; 
