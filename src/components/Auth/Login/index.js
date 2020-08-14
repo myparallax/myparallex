70768dbe02ab4cad383c1091bdceb11f2572de34
Johnsavadkuhi <physitime@gmail.com>
2020-08-15 01:58:03 +0430

    add TextBox and other updates

--- src/components/Auth/Login/index.js
+++ src/components/Auth/Login/index.js
@@ -1,14 +1,13 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup } from "reactstrap";
 import { LOGIN } from "@/services/apollo/query";
 import { useMutation, useApolloClient } from "@apollo/react-hooks";
 import AppContex from "@/components/AppContex";
 import Swal from "sweetalert2";
-import {query } from "@/services/apollo/query";
-
+import { query } from "@/services/apollo/query";
+import TextBox from "../TextBox";
 export default function Login() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { state, setState } = useContext(AppContex);
@@ -16,7 +15,6 @@ export default function Login() {
   const [login, { loading }] = useMutation(LOGIN, {
     errorPolicy: "all",
   });
-
 
   const submitHandle = async (e) => {
     e.preventDefault();
@@ -32,73 +30,69 @@ export default function Login() {
         } else {
           const Toast = Swal.mixin({
             toast: true,
-            position: 'center',
+            position: "center",
             showConfirmButton: false,
             timer: 1000,
             timerProgressBar: true,
             onOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
+              toast.addEventListener("mouseenter", Swal.stopTimer);
+              toast.addEventListener("mouseleave", Swal.resumeTimer);
+            },
+          });
           Toast.fire({
-            icon: 'success',
-            title: 'Signed in successfully'
-          }).then( async () => {
-
-                console.log("data in Swal :  "  ,  ) ;
-                localStorage.setItem("token", await  result.data.signIn.token);
-                client.writeQuery({ query , data: { isLoggedIn: true } });
-                setState({ modalIsOpen: !state.modalIsOpen });
-              });;
+            icon: "success",
+            title: "Signed in successfully",
+          }).then(async () => {
+            console.log("data in Swal :  ");
+            localStorage.setItem("token", await result.data.signIn.token);
+            client.writeQuery({ query, data: { isLoggedIn: true } });
+            setState({ modalIsOpen: !state.modalIsOpen });
+          });
         }
       }
     );
-
   };
 
-    if (loading) return <div> <p style={{padding:'50px'}}>Loading...</p> </div>;
-
+  if (loading)
+    return (
+      <div>
+        {" "}
+        <p style={{ padding: "50px" }}>Loading...</p>{" "}
+      </div>
+    );
 
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
 
   const handleUsername = (e) => {
+    console.log("e.target.value : ", e.target.value);
     setUsername(e.target.value);
   };
 
-
   return (
     <>
-    
       <div className="sign-up">
         <Form className="" onSubmit={submitHandle}>
           <FormGroup>
-            <div className="inputs-text">
-              <img src={require("@/Assests/icons/user.svg")} alt=" " />
-              <Label htmlFor="username"></Label>
-              <Input
-                onChange={handleUsername}
-                value={username}
-                type="text"
-                placeholder="نام کاربری"
-                name="usernmae"
-                className="input-text"
-              />
-            </div>
+            <TextBox
+              iconSrc={require("@/Assests/icons/user.svg")}
+              onChange={handleUsername}
+              value={username}
+              type="text"
+              placeholder="نام کاربری"
+              name="usernmae"
+              className="input-text"
+            />
 
-            <div className="inputs-text">
-              <img src={require("@/Assests/icons/password.svg")} alt=" " />
-              <Label htmlFor="password"></Label>
-              <Input
-                onChange={handlePassword}
-                type="password"
-                placeholder="رمز عبور"
-                name="password"
-                className="input-text"
-              />
-            </div>
+            <TextBox
+              iconSrc={require("@/Assests/icons/password.svg")}
+              onChange={handlePassword}
+              type="password"
+              placeholder="رمز عبور"
+              name="password"
+              className="input-text"
+            />
 
             <div className="btn-container">
               <Button type="submit" className="form-button-login">
@@ -136,9 +130,6 @@ export default function Login() {
           </FormGroup>
         </Form>
       </div>
-   
-
-
     </>
   );
 }
