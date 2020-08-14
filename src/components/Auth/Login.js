3f9dc56c604a655187f27c999da41d8459e685d4
Johnsavadkuhi <physitime@gmail.com>
2020-08-14 21:24:34 +0430

    complete login to connect backend

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,123 +1,126 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import {LOGIN} from '@/services/apollo/query' ; 
-import {useMutation , useApolloClient}from '@apollo/react-hooks' ;
-import { useState } from 'react';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { LOGIN } from "@/services/apollo/query";
+import { useMutation, useApolloClient } from "@apollo/react-hooks";
+import AppContex from "@/components/AppContex";
+import Swal from "sweetalert2";
+import {query } from "@/services/apollo/query";
 
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { state, setState } = useContext(AppContex);
 
-  const [username , setUsername]= useState("") ; 
-  const [password , setPassword] = useState(""); 
+  const client = useApolloClient();
 
-  const client = useApolloClient() ; 
+  const [login, { loading }] = useMutation(LOGIN, {
+    errorPolicy: "all",
+  });
 
-  const [login , {loading , error , data}]  = useMutation(LOGIN , {
-    
-    onCompleted({login}){
-      localStorage.setItem("token" , login ); 
-      client.writeData({data:{isLoggedIn : true }}); 
+  if (loading) return <h2> Loading </h2>;
 
-    }
-  })
-
-  if(loading ) return <h1> Loading </h1>
-  if(error) return <p>an Error occured!</p>
-
-  const submitHandle = e =>{
+  const submitHandle = async (e) => {
     e.preventDefault();
 
-    login({ variables: { login:username , password:password} });
-    
-  } 
+    login({ variables: { login: username, password: password } }).then(
+      (result) => {
+        if (result.data.signIn == null) {
+          const error = result.errors[0].message;
+          Swal.fire({
+            icon: "error",
+            title: "" + error,
+          });
+        } else {
+          Swal.fire({
+            icon: "success",
+            title: "Successfully Signed In !",
+          }).then( async () => {
 
-  const handlePassword = (e) =>{
+                console.log("data in Swal :  "  ,  ) ;
+                localStorage.setItem("token", await  result.data.signIn.token);
+                client.writeQuery({ query , data: { isLoggedIn: true } });
+                setState({ modalIsOpen: !state.modalIsOpen });
+              });;
+        }
+      }
+    );
+  };
 
-    setPassword(e.target.value )
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-  }
-
-  const handleUsername = (e)=>{
-  
-
-    setUsername(e.target.value) 
-  
-  }
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
 
   return (
-
-    <div className="sign-up">
-     
-      <Form className="" onSubmit={submitHandle}>
-       
-        <FormGroup>
-          <div className="inputs-text">
-              <img src={require('@/Assests/icons/user.svg')} alt=" "/>
-            <Label for="username"></Label>
-            <Input
-            
-                onChange = {handleUsername} 
+    <>
+      <div className="sign-up">
+        <Form className="" onSubmit={submitHandle}>
+          <FormGroup>
+            <div className="inputs-text">
+              <img src={require("@/Assests/icons/user.svg")} alt=" " />
+              <Label for="username"></Label>
+              <Input
+                onChange={handleUsername}
                 value={username}
                 type="text"
                 placeholder="نام کاربری"
                 name="usernmae"
                 className="input-text"
-            />
-          </div>
+              />
+            </div>
 
-          <div className="inputs-text">
-            <img src={require('@/Assests/icons/password.svg')} alt=" "/>
-            <Label for="password"></Label>
-            <Input
-                onChange = {handlePassword} 
+            <div className="inputs-text">
+              <img src={require("@/Assests/icons/password.svg")} alt=" " />
+              <Label for="password"></Label>
+              <Input
+                onChange={handlePassword}
                 type="password"
                 placeholder="رمز عبور"
                 name="password"
                 className="input-text"
-            />
-          </div>
+              />
+            </div>
 
-          <div className="btn-container">
-            <Button
-            
-              type="submit"
-              className="form-button-login"
-            >
-              ورود
-            </Button>
-          </div>
-         
-          <div className="login-links-container">
-            <div className="round">
-              <span>{'من را به خاطر بسپار'}</span>
-              <input type="checkbox" id="checkbox" />
-              <label htmlFor="checkbox"></label>
+            <div className="btn-container">
+              <Button type="submit" className="form-button-login">
+                ورود
+              </Button>
             </div>
-            <div>
-              <Link to="" className="forgot-pass">
-                رمز عبور خود را فراموش کرده اید؟
-              </Link>
+
+            <div className="login-links-container">
+              <div className="round">
+                <span>{"من را به خاطر بسپار"}</span>
+                <input type="checkbox" id="checkbox" />
+                <label htmlFor="checkbox"></label>
+              </div>
+              <div>
+                <Link to="" className="forgot-pass">
+                  رمز عبور خود را فراموش کرده اید؟
+                </Link>
+              </div>
             </div>
-          </div>
-          
-          <div className="social-accounts">
-            <div className="base-line"></div>
-            <span className="login-text-social">or login with</span>
-            <div className="social-icons">
-              <button className="icons">
-                <span>Github</span>
-                <img src={require('@/Assests/icons/github.svg')} alt=" "/>
-              </button>
-              <button className="icons">
-                <span>Google</span>
-                <img src={require('@/Assests/icons/google.svg')} alt=" "/>
-              </button>
+
+            <div className="social-accounts">
+              <div className="base-line"></div>
+              <span className="login-text-social">or login with</span>
+              <div className="social-icons">
+                <button className="icons">
+                  <span>Github</span>
+                  <img src={require("@/Assests/icons/github.svg")} alt=" " />
+                </button>
+                <button className="icons">
+                  <span>Google</span>
+                  <img src={require("@/Assests/icons/google.svg")} alt=" " />
+                </button>
+              </div>
             </div>
-          </div>
-          
-        </FormGroup>
-      </Form>
-      
-    </div>
+          </FormGroup>
+        </Form>
+      </div>
+    </>
   );
 }
