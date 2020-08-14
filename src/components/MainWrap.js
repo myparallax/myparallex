efa44bbfea034af8ad638bfb54ca9334e9ab9b07
Johnsavadkuhi <physitime@gmail.com>
2020-08-14 21:23:02 +0430

    update

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -3,12 +3,12 @@ import Home from './Home';
 import { BrowserRouter  , Route , Switch } from 'react-router-dom'   
 import Header from './Header' ;
 import Dashboard from "./Dashboard";
-
 function MainWrap() {
 
     return  <BrowserRouter >
        
         <Header/>
+
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/dashboard" component={Dashboard}/>
