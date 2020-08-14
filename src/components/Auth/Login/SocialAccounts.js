f01bdab7488c3ef9d9f1692572e9f441c342f654
Johnsavadkuhi <physitime@gmail.com>
2020-08-15 02:08:18 +0430

    remove props and format code

--- src/components/Auth/Login/SocialAccounts.js
+++ src/components/Auth/Login/SocialAccounts.js
@@ -1,25 +1,24 @@
-import React from 'react'; 
+import React from "react";
 
-
-function SocialAccounts(props){
-
-
-return <>
-<div className="social-accounts">
-              <div className="base-line"></div>
-              <span className="login-text-social">or login with</span>
-              <div className="social-icons">
-                <button className="icons">
-                  <span>Github</span>
-                  <img src={require("@/Assests/icons/github.svg")} alt=" " />
-                </button>
-                <button className="icons">
-                  <span>Google</span>
-                  <img src={require("@/Assests/icons/google.svg")} alt=" " />
-                </button>
-              </div>
-            </div>
-</>
+function SocialAccounts() {
+  return (
+    <>
+      <div className="social-accounts">
+        <div className="base-line"></div>
+        <span className="login-text-social">or login with</span>
+        <div className="social-icons">
+          <button className="icons">
+            <span>Github</span>
+            <img src={require("@/Assests/icons/github.svg")} alt=" " />
+          </button>
+          <button className="icons">
+            <span>Google</span>
+            <img src={require("@/Assests/icons/google.svg")} alt=" " />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default SocialAccounts ; 
+export default SocialAccounts;
