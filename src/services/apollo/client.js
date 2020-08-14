2c3f0eb989e0fc65ff37eccb0557407e65607d5c
Johnsavadkuhi <physitime@gmail.com>
2020-08-14 21:25:41 +0430

    deep update

--- src/services/apollo/client.js
+++ src/services/apollo/client.js
@@ -1,28 +1,53 @@
-import ApolloClient, { HttpLink } from 'apollo-boost';
-import {InMemoryCache} from 'apollo-boost';
 import { resolvers, typeDefs } from "./resolvers";
+import { onError } from "apollo-link-error" ;
+import {  ApolloClient , from  } from '@apollo/client';
+import {HttpLink } from 'apollo-link-http'  ; 
+ import {InMemoryCache} from 'apollo-cache-inmemory' ; 
 
-// const uri  = process.env.URI 
-const uri = "http://localhost:4000" ; 
-console.log("uri : " +  uri );
+const uri = "http://localhost:4000"; 
+ 
+
 const cache = new InMemoryCache() ; 
 
+const errorLink = onError(({ graphQLErrors, networkError , operation, forward }) => {
+   
+  if (graphQLErrors)
+
+    graphQLErrors.map(({ message, locations, path }) =>
+      
+    console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+
+});
+
+
+const httpLink = new HttpLink({uri , 
+  headers:{authorization:localStorage.getItem("token")} , 
+   
+}); 
+
 const client = new ApolloClient({
-  link : new HttpLink({
-    headers:{authorization:localStorage.getItem("token")}
-  }),
-    uri , 
-    cache , 
+  
+    
+    cache ,
+    link : from([errorLink , httpLink ])   , 
     typeDefs,
-    resolvers
+    resolvers, 
+    
   });
 
+  
   cache.writeData({
     data: {
       isLoggedIn: !!localStorage.getItem('token'),
       
     },
   });
+
 export default client 
 
 
