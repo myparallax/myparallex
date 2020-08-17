c8a7cfddaa3c7f5e827865c8765927c9f48f08e1
Johnsavadkuhi <physitime@gmail.com>
2020-08-17 08:13:12 +0430

    remove props

--- src/components/Auth/Login/Submit.js
+++ src/components/Auth/Login/Submit.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button  } from "reactstrap";
 import {Link} from 'react-router-dom' ; 
 
-function Submit(props) {
+function Submit() {
   return (
     <>
       <div className="btn-container">
