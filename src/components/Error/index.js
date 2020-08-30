5d2f037622e5e6347e5d36a5087b1a19ed8e94ca
Johnsavadkuhi <physitime@gmail.com>
2020-08-30 23:59:28 +0430

    update

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -6,14 +6,11 @@ class ErrorBoundary extends React.Component {
     this.state = { hasError: false };
   }
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
   componentDidCatch(error, info) {
-    // You can also log the error to an error reporting service
     console.error("Error :" + Error);
     console.info("info : " + info);
-    // logErrorToMyService(error, info);
   }
   render() {
     if (this.state.hasError) {
