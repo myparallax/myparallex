b4cdd68ba40031ad65134474d885bb6f6096158c
Johnsavadkuhi <physitime@gmail.com>
2020-08-30 23:54:55 +0430

    update

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,28 +1,38 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import Login from './Login';
-import SignUp from './SignUp'
-import ErrorBoundary from '@/components/Error' ; 
+import Login from "./Login";
+import SignUp from "./SignUp";
+import ErrorBoundary from "@/components/Error";
 
 export default function Auth(props) {
-
   const [login, setLogin] = useState(props.login);
-  
+
   return (
-
     <div className="auth-container">
+      <div className="auth-btns">
+        <button
+          onClick={() => setLogin(true)}
+          className={`auth-btn-1 ${login ? "active" : ""} `}
+        >
+          ورود
+        </button>
+        <button
+          onClick={() => setLogin(false)}
+          className={`${!login ? "active" : ""} `}
+        >
+          ثبت نام
+        </button>
+      </div>
 
-        <div className="auth-btns">
-            <button  onClick={() => setLogin(true)} className={`auth-btn-1 ${login ? "active" : "" } `}>ورود</button>
-            <button onClick={()=> setLogin(false)} className={`${!login ? "active" : "" } `}>ثبت نام</button> 
-        </div>  
-       
-        {
-            login 
-            ? <ErrorBoundary><Login/></ErrorBoundary>
-            : <ErrorBoundary><SignUp /></ErrorBoundary>
-        }
-
+      {login ? (
+        <ErrorBoundary>
+          <Login />
+        </ErrorBoundary>
+      ) : (
+        <ErrorBoundary>
+          <SignUp />
+        </ErrorBoundary>
+      )}
     </div>
   );
 }
