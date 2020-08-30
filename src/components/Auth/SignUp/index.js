7056a32aa141773474c4635bcff40c7cc17e0b7d
Johnsavadkuhi <physitime@gmail.com>
2020-08-30 23:55:21 +0430

    update

--- src/components/Auth/SignUp/index.js
+++ src/components/Auth/SignUp/index.js
@@ -1,66 +1,61 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-
   return (
     <div className="sign-up">
-      
-      <Form className="" >
-       
+      <Form className="">
         <FormGroup>
-          
           <div className="inputs-text">
-              <img src={require('@/Assests/icons/email.svg')} alt=" "/>
+            <img src={require("@/Assests/icons/email.svg")} alt=" " />
             <Label for="username"></Label>
             <Input
-                type="email"
-                id="email"
-                placeholder="پست الکترونیک"
-                name="email"
-                className="input-text"
+              type="email"
+              id="email"
+              placeholder="پست الکترونیک"
+              name="email"
+              className="input-text"
             />
           </div>
-        
 
           <div className="inputs-text">
-            <img src={require('@/Assests/icons/password.svg')} alt=" "/>
+            <img src={require("@/Assests/icons/password.svg")} alt=" " />
             <Label for="password"></Label>
             <Input
-                type="password"
-                id="password"
-                placeholder="رمز عبور"
-                name="password"
-                className="input-text"
+              type="password"
+              id="password"
+              placeholder="رمز عبور"
+              name="password"
+              className="input-text"
             />
           </div>
 
           <div className="btn-container">
-            <Button
-              type="submit"
-              className="form-button-login"
-            >
-           ثبت نام 
+            <Button type="submit" className="form-button-login">
+              ثبت نام
             </Button>
           </div>
-         
+
           <div className="container login-links-container">
             <div className="round">
-              <span>{'به روزرسانی ها را برای من ارسال کنید'}</span>
+              <span>{"به روزرسانی ها را برای من ارسال کنید"}</span>
               <input type="checkbox" id="checkbox" />
               <label htmlFor="checkbox"></label>
             </div>
             <div className="contact-us">
-              برای فعال کردن حساب خود یک ایمیل تائید در صندوق ورودی خود با پیوند دریافت خواهید کرد
-               اگر مشکلی دارید
-              <Link to="#" variant="body2" style={{fontFamily:'iran-sans' ,fontSize:'10px'}}>
-              با ما تماس  
-              </Link> بگیرید.
+              برای فعال کردن حساب خود یک ایمیل تائید در صندوق ورودی خود با پیوند
+              دریافت خواهید کرد اگر مشکلی دارید
+              <Link
+                to="#"
+                variant="body2"
+                style={{ fontFamily: "iran-sans", fontSize: "10px" }}
+              >
+                با ما تماس
+              </Link>{" "}
+              بگیرید.
             </div>
           </div>
-          
-          
 
           <div className="social-accounts">
             <div className="base-line"></div>
@@ -68,18 +63,16 @@ export default function Login() {
             <div className="social-icons">
               <button className="icons">
                 <span>Github</span>
-                <img src={require('@/Assests/icons/github.svg')} alt=" "/>
+                <img src={require("@/Assests/icons/github.svg")} alt=" " />
               </button>
               <button className="icons">
                 <span>Google</span>
-                <img src={require('@/Assests/icons/google.svg')} alt=" "/>
+                <img src={require("@/Assests/icons/google.svg")} alt=" " />
               </button>
             </div>
           </div>
-          
         </FormGroup>
       </Form>
-      
     </div>
   );
 }
