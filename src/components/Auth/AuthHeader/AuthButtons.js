d4507284eded0db04e55fd19af0b9ca71efa53ee
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 04:18:02 +0430

    create component

---
+++ src/components/Auth/AuthHeader/AuthButtons.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+
+function AuthButtons(props){
+
+    return <>
+          <div className="auth-btns">{props.children}</div>
+    
+    </>
+}
+
+export default AuthButtons ; 
