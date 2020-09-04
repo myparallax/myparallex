d90ec3aaafd743fbd7b32d487e6265ee998f2c7d
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 00:58:56 +0430

    add AuthHeader component

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -3,28 +3,16 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import ErrorBoundary from "@/components/Error";
 import CheckInternet from '@/components/Network/CheckInternet'; 
+import AuthHeader from "./AuthHeader";
 
 export default function Auth(props) {
   const [login, setLogin] = useState(props.login);
 
   return (
     <div className="auth-container">
+      
      <CheckInternet>
-      <div className="auth-btns">
-        <button
-          onClick={() => setLogin(true)}
-          className={`auth-btn-1 ${login ? "active" : ""} `}
-        >
-          ورود
-        </button>
-        <button
-          onClick={() => setLogin(false)}
-          className={`${!login ? "active" : ""} `}
-        >
-          ثبت نام
-        </button>
-      </div>
-
+       <AuthHeader login={login} setLogin={setLogin} />
       {login ? (
         <ErrorBoundary>
           <Login />
