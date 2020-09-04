3b1beb75d438e41e9369c506254e729dc1d8d922
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 00:37:33 +0430

    add fragment element

--- src/components/MainWrap.js
+++ src/components/MainWrap.js
@@ -1,22 +1,21 @@
 import React from "react";
-import Home from './Home';
-import { BrowserRouter  , Route , Switch } from 'react-router-dom'   
-import Header from './Header' ;
+import Home from "./Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Header";
 import Dashboard from "./Dashboard";
 function MainWrap() {
-
-    return  <BrowserRouter >
-       
-        <Header/>
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
 
         <Switch>
-            <Route path="/" exact component={Home}/>
-            <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/dashboard" component={Dashboard} />
         </Switch>
-    
-    </BrowserRouter>
-   
-
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default MainWrap;
