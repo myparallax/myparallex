e011b26a38073ccb02c460d7d75b8d8d2943e060
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 00:36:57 +0430

    move modal code to modalheader component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,10 @@
 import React, { useState, useContext } from "react";
-import { Modal, ModalBody, Input } from "reactstrap";
-import Auth from "../Auth";
 import { IS_LOGGED_IN, query } from "@/services/apollo/query";
 import { useQuery } from "@apollo/client";
 import AppContext from "@/components/AppContex";
 import { useApolloClient } from "@apollo/client";
 import NavbarHeader from './NavbarHeader/NavbarHeader'; 
+import ModalHeader from './ModalHeader'; 
 
 function Header() {
 
@@ -38,51 +37,17 @@ function Header() {
     cache.writeQuery({ query, data: { isLoggedIn: false } });
     localStorage.clear();
   };
+  
   const NavbarHeaderProps = {data , toggleCollapse , sidebarOpened , toggleModal , handleLogout , toggleModalSearch , collapseOpen}
+  const ModalHeaderProps = {modalSearch , toggleModalSearch ,state , toggleModal , loginOrReagister  }; 
 
   return (
     <>
     
       <NavbarHeader {...NavbarHeaderProps}/>
 
-      <Modal
-        modalClassName="modal-search"
-        isOpen={modalSearch}
-        toggle={toggleModalSearch}
-      >
-        <div className="modal-header">
-          <Input
-            id="inlineFormInputGroup"
-            placeholder="جستجو ..."
-            type="text"
-          />
-          <button
-            aria-label="Close"
-            className="blue-btn"
-            data-dismiss="modal"
-            type="button"
-            onClick={toggleModalSearch}
-          >
-            <img
-              className="icon-simple-remove"
-              src={require("@/Assests/remove.svg")}
-              alt="remove"
-            />
-          </button>
-        </div>
-      </Modal>
-     
-      <Modal
-        isOpen={state.modalIsOpen}
-        modalClassName="modal-auth"
-        centered
-        toggle={toggleModal}
-      >
-        <ModalBody>
-          <Auth login={loginOrReagister} />
-        </ModalBody>
-      </Modal>
-   
+      <ModalHeader {...ModalHeaderProps}/>
+    
     </>
   );
 }
