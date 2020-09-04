0688215643c98a71bfb82b27ae4926ab8fa72536
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 00:37:15 +0430

    add modal code

--- src/components/Header/ModalHeader.js
+++ src/components/Header/ModalHeader.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Modal, ModalBody, Input } from "reactstrap";
+import Auth from "../Auth";
 
-function Modalheader() {
-  return <div></div>;
+function Modalheader({modalSearch , toggleModalSearch ,state , toggleModal , loginOrReagister  }) {
+
+  return (
+    <>
+      <Modal
+        modalClassName="modal-search"
+        isOpen={modalSearch}
+        toggle={toggleModalSearch}
+      >
+        <div className="modal-header">
+          <Input
+            id="inlineFormInputGroup"
+            placeholder="جستجو ..."
+            type="text"
+          />
+          <button
+            aria-label="Close"
+            className="blue-btn"
+            data-dismiss="modal"
+            type="button"
+            onClick={toggleModalSearch}
+          >
+            <img
+              className="icon-simple-remove"
+              src={require("@/Assests/remove.svg")}
+              alt="remove"
+            />
+          </button>
+        </div>
+      </Modal>
+
+      <Modal
+        isOpen={state.modalIsOpen}
+        modalClassName="modal-auth"
+        centered
+        toggle={toggleModal}
+      >
+        <ModalBody>
+          <Auth login={loginOrReagister} />
+        </ModalBody>
+      </Modal>
+    </>
+  );
 }
 
 export default Modalheader;
