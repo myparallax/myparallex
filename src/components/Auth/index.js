d645df0c4d378b3679496611d287cbb8b6d7fce7
Johnsavadkuhi <physitime@gmail.com>
2020-09-05 04:18:15 +0430

    add AuthHeader

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -3,7 +3,7 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import ErrorBoundary from "@/components/Error";
 import CheckInternet from '@/components/Network/CheckInternet'; 
-import AuthHeader from "./AuthHeader";
+import AuthHeader from "./AuthHeader/AuthHeader";
 
 export default function Auth(props) {
   const [login, setLogin] = useState(props.login);
