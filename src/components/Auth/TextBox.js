992d010573b315f229045a3fcd58d535744ba65e
Johnsavadkuhi <physitime@gmail.com>
2020-09-25 23:17:43 +0330

    update

--- src/components/Auth/TextBox.js
+++ src/components/Auth/TextBox.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Label, Input } from "reactstrap";
 
 function TextBox(props) {
-  
-  const {iconSrc , ...restProps} = props ; 
+  const { iconSrc, ...restProps } = props;
 
   return (
     <>
       <div className="inputs-text">
         <img src={iconSrc} alt="parallex img" />
         <Label htmlFor={props.type === "email" ? "email" : "password"}></Label>
-        <Input
-        {...restProps}
-/>
+        <Input {...restProps} />
       </div>
     </>
   );
